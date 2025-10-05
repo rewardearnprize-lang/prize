cc72fe929b68ba0bb05a617d22fdb40022bca0fb
rewardearnprize-lang <rewardearnprize@gmail.com>
2025-10-05 21:25:32 +0300

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function generateUserKey(): string {
+  const timestamp = Date.now().toString(36); // جزء من الوقت
+  const randomPart = Math.random().toString(36).substring(2, 10); // جزء عشوائي
+  return `key_${timestamp}_${randomPart}`;
+}
